23add4ae6d5ff86565798277697ce4b08e4bb636
Anna <iaanya27@gmail.com>
2024-07-21 14:52:13 +0300

    added task 1 without css

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,33 @@
+import styles from "./Profile.module.css"
+
+
+export const Profile = ({ name, tag, location, image, stats }) => {
+    return (
+        <div className={styles.card}>
+            <div>
+                <img
+                    src={image}
+                    alt="User avatar"
+                />
+                <p>{name}</p>
+                <p>{tag}</p>
+                <p>{location}</p>
+            </div>
+
+            <ul>
+                <li>
+                    <span>Followers</span>
+                    <span>{stats.followers}</span>
+                </li>
+                <li>
+                    <span>Views</span>
+                    <span>{stats.views}</span>
+                </li>
+                <li>
+                    <span>Likes</span>
+                    <span>{stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
