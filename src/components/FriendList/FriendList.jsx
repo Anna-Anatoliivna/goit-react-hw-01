c93dd89812c4f6a25fa04dac7250acc99311703f
Anna <iaanya27@gmail.com>
2024-07-22 15:18:35 +0300

    added task3

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,11 @@
-import styles from "./FriendList.module.css"
+// import styles from "./FriendList.module.css"
 
-export const FriendList = ({ avatar, name, isOnline }) => {
-    return (<div className={styles.card}>
-        <img src={avatar} alt="Avatar" width="48" />
-        <p>{name}</p>
-        <p>{isOnline}</p>
-    </div >)
-}
+// export const FriendList = ({ friends }) => {
+//     return (
+//         <div className={styles.card} key={id}>
+//             {friends.map(({ avatar, name, isOnline }) => (
+//                 <img src={avatar} alt="Avatar" width="48" />
+//         <p>{name}</p>
+//         <p>{isOnline}</p>            
+//                 </div>)
+//            ) }
